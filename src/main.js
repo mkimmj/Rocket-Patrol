d6c678762ae748df5b2bb4eebaa107d50cf2c711
Michelle Kim <mkim139@ucsc.edu>
2022-04-12 09:06:30 -0700

    final commit

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ let config = {
     height: 480,
     scene: [ Menu, Play ]
   }
-  
+
 let game = new Phaser.Game(config);
 
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
